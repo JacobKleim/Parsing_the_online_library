48bf327666d4fee1fdc74eac91616b391125d770
JacobKleim <yakovkleimenov@yandex.ru>
2024-01-13 21:46:21 +0400

    Created a separate function to parse the page

--- main.py
+++ main.py
@@ -33,48 +33,69 @@ def download_image(url, folder='images/'):
     response.raise_for_status()
     parsed_url = urlsplit(url)
     decoded_filename = unquote(parsed_url.path.split('/')[-1])
-    print(path)
-    print(decoded_filename)
+    # print(path)
+    # print(decoded_filename)
     filepath = os.path.join(path, decoded_filename)
 
     with open(filepath, 'wb') as file:
         file.write(response.content)
     return filepath
-from itertools import islice
 
-for book_id in range(7, 11):
-    text_url = f'https://tululu.org/txt.php?id={book_id}'
-    book_page_url = f'https://tululu.org/b{book_id}/'
-    text_response = requests.get(text_url)
-    text_response.raise_for_status()
-    book_response = requests.get(book_page_url)
-    book_response.raise_for_status()
-    try:
-        check_for_redirect(text_response)
-        soup = BeautifulSoup(book_response.text, 'lxml')
-        page_title = soup.find('body').find('table').find(
-            'td', class_='ow_px_td').find('div').find('h1')
-        page_title_text = page_title.text
-        page_title_splited_text = page_title_text.split('::')
-        book_title = page_title_splited_text[0]
-        author = page_title_splited_text[1]
-        page_comments = soup.find('body').find('table').find(
-            'td', class_='ow_px_td').find_all('div', class_='texts')
-        comments_text = '\n'.join(
-            [comment.get_text().split(')', 1)[1].strip()
-             for comment in page_comments])
-        print(comments_text)
-        print(book_title)
-        image_path = soup.find('body').find('table').find(
-            'div', class_='bookimage').find('img')['src']
-        image_url = urljoin('https://tululu.org/', image_path)
-        book_genres = soup.find('body').find('table').find(
-            'span', class_='d_book').find_all('a')
-        genres = ([genre.get_text() for genre in book_genres])
-        print(genres)
-        
-        # print(image_url)
-        # download_image(image_url)
-        download_txt(text_url, book_title)
-    except requests.HTTPError:
-        print(f'HTTPError: {text_response.history}')
+
+def parse_book_page(soup):
+    book_page = {}
+    page_title = soup.find('body').find('table').find(
+        'td', class_='ow_px_td').find('div').find('h1')
+    page_title_text = page_title.text
+    page_title_splited_text = page_title_text.split('::')
+    book_title = page_title_splited_text[0].strip()
+    author = page_title_splited_text[1].strip()
+    page_comments = soup.find('body').find('table').find(
+        'td', class_='ow_px_td').find_all('div', class_='texts')
+    comments_text = '; '.join(
+        [comment.get_text().split(')', 1)[1].strip()
+            for comment in page_comments])
+    image_path = soup.find('body').find('table').find(
+        'div', class_='bookimage').find('img')['src']
+    image_url = urljoin('https://tululu.org/', image_path)
+
+    book_genres = soup.find('body').find('table').find(
+        'span', class_='d_book').find_all('a')
+    genre = ([genre.get_text() for genre in book_genres])
+    # print(author)
+    # print(book_title)
+    # print(genre)
+    # print(image_url)
+    # print(comments_text)
+    # print('\n')
+    book_page = {'author': author,
+                 'book_title': book_title,
+                 'comments_text': comments_text,
+                 'image_url': image_url,
+                 'genre': genre
+    }
+
+    return book_page
+
+
+def main():
+    for book_id in range(9, 11):
+        text_url = f'https://tululu.org/txt.php?id={book_id}'
+        book_page_url = f'https://tululu.org/b{book_id}/'
+        text_response = requests.get(text_url)
+        text_response.raise_for_status()
+        book_response = requests.get(book_page_url)
+        book_response.raise_for_status()
+        try:
+            check_for_redirect(text_response)
+            soup = BeautifulSoup(book_response.text, 'lxml')
+            parsed_book = parse_book_page(soup)
+            print(parsed_book)
+            download_image(parsed_book['image_url'])
+            download_txt(text_url, parsed_book['book_title'])
+        except requests.HTTPError:
+            print(f'HTTPError: {text_response.history}')
+
+
+if __name__ == '__main__':
+    main()

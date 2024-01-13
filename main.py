1464ade150c0cfef426eea924c2c1ba73f49f3b3
JacobKleim <yakovkleimenov@yandex.ru>
2024-01-13 19:09:28 +0400

    Download comments

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def download_image(url, folder='images/'):
     return filepath
 
 
-for book_id in range(1, 11):
+for book_id in range(7, 11):
     text_url = f'https://tululu.org/txt.php?id={book_id}'
     book_page_url = f'https://tululu.org/b{book_id}/'
     text_response = requests.get(text_url)
@@ -51,20 +51,25 @@ for book_id in range(1, 11):
     book_response.raise_for_status()
     try:
         check_for_redirect(text_response)
-        title_soup = BeautifulSoup(book_response.text, 'lxml')
-        page_title = title_soup.find('body').find('table').find(
+        soup = BeautifulSoup(book_response.text, 'lxml')
+        page_title = soup.find('body').find('table').find(
             'td', class_='ow_px_td').find('div').find('h1')
         page_title_text = page_title.text
         page_title_splited_text = page_title_text.split('::')
         book_title = page_title_splited_text[0]
         author = page_title_splited_text[1]
-        print(book_title)
-        image_soup = BeautifulSoup(book_response.text, 'lxml')
-        image_path = image_soup.find('body').find('table').find(
+        page_comments = soup.find('body').find('table').find(
+            'td', class_='ow_px_td').find_all('div', class_='texts')
+        comments_text = '\n'.join(
+            [comment.get_text().split(')', 1)[1].strip()
+             for comment in page_comments])
+        print(comments_text)
+        # print(book_title)
+        image_path = soup.find('body').find('table').find(
             'div', class_='bookimage').find('img')['src']
         image_url = urljoin('https://tululu.org/', image_path)
-        print(image_url)
-        download_image(image_url)
-        download_txt(text_url, book_title)
+        # print(image_url)
+        # download_image(image_url)
+        # download_txt(text_url, book_title)
     except requests.HTTPError:
-        print(f"HTTPError: {text_response.history}")
+        print(f'HTTPError: {text_response.history}')
